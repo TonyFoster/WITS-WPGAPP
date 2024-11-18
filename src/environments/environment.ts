942db45bb183dcf2fd27b06df251d36ea287cbf5
Tony <tony19907051@gmail.com>
2024-11-18 23:35:00 +0800

    Add environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBSo1jLzo9n7-N2uuYOAO7n5-gDax0O5Lc",
+    authDomain: "mcark-55e54.firebaseapp.com",
+    projectId: "mcark-55e54",
+    storageBucket: "mcark-55e54.firebasestorage.app",
+    messagingSenderId: "860357315745",
+    appId: "1:860357315745:web:bad8bba7efbb655ddff4e0",
+    measurementId: "G-DV45Y73H32"
+  },
+};
