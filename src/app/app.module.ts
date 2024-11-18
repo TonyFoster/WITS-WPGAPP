97668b0117d491423686ffe3ae0c7cc191c4ef38
Tony <tony19907051@gmail.com>
2024-11-19 01:09:19 +0800

    Add PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,17 @@
-import {NgModule} from '@angular/core';
+import {NgModule, isDevMode} from '@angular/core';
 import {BrowserModule, provideClientHydration} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {NameSelectorModalComponent} from './name-selector-modal/name-selector-modal.component';
+import {AttendanceRecordsComponent} from './attendance-records/attendance-records.component';
+import {HomeComponent} from './home/home.component';
+import {HoldButtonComponent} from './home/hold-button/hold-button.component';
+import {FormsModule} from '@angular/forms';
 import {CookieService} from 'ngx-cookie-service';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {NameSelectorModalComponent} from './name-selector-modal/name-selector-modal.component';
-import {MatFormField, MatLabel} from '@angular/material/form-field';
-import {MatOption, MatSelect} from '@angular/material/select';
-import {FormsModule} from '@angular/forms';
-import {MatButton} from '@angular/material/button';
 import {provideHttpClient} from '@angular/common/http';
-import {AttendanceRecordsComponent} from './attendance-records/attendance-records.component';
-import { HomeComponent } from './home/home.component';
-import { HoldButtonComponent } from './home/hold-button/hold-button.component';
 
 @NgModule({
   declarations: [
@@ -26,12 +24,13 @@ import { HoldButtonComponent } from './home/hold-button/hold-button.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatFormField,
-    MatSelect,
-    FormsModule,
-    MatOption,
-    MatButton,
-    MatLabel
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    FormsModule
   ],
   providers: [
     provideClientHydration(),
